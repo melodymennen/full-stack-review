f8211b96fa71c630177f76ba33667ff5224457a5
Melody <Melody@Melodys-MacBook.local>
2018-01-11 10:53:22 -0700

    home, acct info components

---
+++ src/components/AcctInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AcctInfo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>AcctInfo</h1>
+            </div>
+        );
+    }
+}
+
+export default AcctInfo;
