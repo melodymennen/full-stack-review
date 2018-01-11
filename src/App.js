bcdbfa91aca7f68713cbd0b89daec03315b42255
Melody <Melody@Melodys-MacBook.local>
2018-01-11 10:48:08 -0700

    some setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Route } from 'react-router-dom';
-import Home from './components/Home';
 import AcctInfo from './components/AcctInfo';
+import Home from './components/Home';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path='/' component={Home} />
-        <Route path='/private' component={AcctInfo} />
+        <Route exact path="/" component={Home} />
+        <Route path="/private" component={AcctInfo} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
 import { HashRouter } from 'react-router-dom';
+// import registerServiceWorker from './registerServiceWorker';
 
 
 
